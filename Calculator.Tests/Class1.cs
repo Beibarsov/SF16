bae7c178e59cbfb5d3589bc253468160b8a1ee20
Beibarsov <BarsStudio@gmail.com>
2022-04-20 19:55:54 +0800

    Добавьте файлы проекта.

---
+++ Calculator.Tests/Class1.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using Practices;
+
+
+namespace SimpleCalculator.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+
+        Calculator calc = new Calculator();
+        [Test]
+        public void AdditionalMustReturnTrueValue()
+        {
+
+            Assert.True(calc.Additional(5, 5) == 10);
+        }
+
+
+        [Test]
+        public void SubtractionMustReturnTrueValue()
+        {
+
+            Assert.True(calc.Subtraction(5, 5) == 0);
+        }
+
+        [Test]
+        public void MultiplicationMustReturnTrueValue()
+        {
+
+            Assert.True(calc.Miltiplication(5, 5) == 25);
+        }
+
+        [Test]
+        public void DivisionMustReturnTrueValue()
+        {
+
+            Assert.True(calc.Division(10, 5) == 2);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ SF16/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Practices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+
+
+}
+
+
+
